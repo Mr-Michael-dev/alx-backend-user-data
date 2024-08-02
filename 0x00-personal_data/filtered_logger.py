cde60268287c501763fa8bb19f369fcf6a6ebd6a
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-02 03:51:11 +0100

    debug filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,5 @@ import re
 
 def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     """Obfuscates a log message and return it"""
-    pattern = fr'({"|".join(fields)})=[^{separator}]+u
+    pattern = fr'({"|".join(fields)})=[^{separator}]+'
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
