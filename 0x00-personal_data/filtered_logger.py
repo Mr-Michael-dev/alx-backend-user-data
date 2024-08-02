a8ae8a1041b464530c0cd59a015f732997e2f62f
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-02 05:04:29 +0100

    debug formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,41 +1,66 @@
 #!/usr/bin/env python3
 """
 Contains a function called filter_datum that returns
-the log message obfuscated
+the log message obfuscated.
 """
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields: list,
+def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
-    """Obfuscates a log message and return it"""
+    """
+    Obfuscates a log message and returns it.
+
+    Args:
+        fields (List[str]): List of fields to obfuscate.
+        redaction (str): String to replace field values.
+        message (str): Log message to be obfuscated.
+        separator (str): Character separating fields in the log message.
+
+    Returns:
+        str: Obfuscated log message.
+    """
     pattern = fr'({"|".join(fields)})=[^{separator}]+'
     return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}', message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
+    """
+    Redacting Formatter class.
+    This class inherits from logging.Formatter and is used to obfuscate
+    sensitive information in log messages.
+    """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list):
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the RedactingFormatter.
+
+        Args:
+            fields (List[str]): List of fields to obfuscate.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
-        formats a log record data using the filter_datum method
+        Format a log record's data using the filter_datum method.
+
+        Args:
+            record (logging.LogRecord): Log record to be formatted.
+
+        Returns:
+            str: Formatted and obfuscated log message.
         """
         message = record.getMessage()
         record.msg = filter_datum(self.fields,
                                   self.REDACTION,
                                   message,
                                   self.SEPARATOR)
-
         return super(RedactingFormatter, self).format(record)
