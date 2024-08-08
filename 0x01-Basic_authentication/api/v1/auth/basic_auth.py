4ea006105c8f1e64848d433d7c7647df86411a49
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-08 19:37:44 +0100

    decode Basic - Base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,28 @@ class BasicAuth(Auth):
         encoded_credentials = parts[1]
 
         return encoded_credentials
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        Decode the base64 encoded username and password
+
+        Arguments:
+        base64_authorization_header {str} - encoded Base64 string
+
+        Returns:
+        decoded value as UTF8 string
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_credentials = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decoded_credentials
+        except Exception as e:
+            return None
