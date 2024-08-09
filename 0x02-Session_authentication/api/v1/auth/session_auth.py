74b140d2aa3448508e1dac8395b7c0e2714c3891
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-09 21:11:45 +0100

    retrieve user_id base on session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,3 +31,16 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[str(session_id)] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns the user_id for a given session_id
+        """
+        if session_id is None:
+            return None
+
+        if not isinstance(session_id, str):
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
--- 0x02-Session_authentication/main_1.py
+++ 0x02-Session_authentication/main_1.py
@@ -1,28 +1,48 @@
-#!/usr/bin/env python3
-""" Main 1
+#!/usr/bin/python3
+""" Check response
 """
-from api.v1.auth.session_auth import SessionAuth
 
-sa = SessionAuth()
+if __name__ == "__main__":
 
-print("{}: {}".format(type(sa.user_id_by_session_id), sa.user_id_by_session_id))
+    try:
+        from api.v1.auth.session_auth import SessionAuth
+        sa = SessionAuth()
+        user_id_1 = "User 1"
+        session_id_1 = sa.create_session(user_id_1)
+        if session_id_1 is None:
+            print("create_session doesn't return a session ID when user_id is a string")
+            exit(1)
+        if sa.user_id_by_session_id is None:
+            print("user_id_by_session_id should not be None")
+            exit(1)
+        if type(sa.user_id_by_session_id) is not dict:
+            print("user_id_by_session_id should be a dictionary: {}".format(sa.user_id_by_session_id))
+            exit(1)
+        r_user_id_1 = sa.user_id_by_session_id.get(session_id_1)
+        if r_user_id_1 != user_id_1:
+            print("User doesn't found for this session ID: {} -> {}".format(session_id_1, sa.user_id_by_session_id))
+            exit(1)
 
-user_id = None
-session = sa.create_session(user_id)
-print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
-
-user_id = 89
-session = sa.create_session(user_id)
-print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
-
-user_id = "abcde"
-session = sa.create_session(user_id)
-print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
-
-user_id = "fghij"
-session = sa.create_session(user_id)
-print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
-
-user_id = "abcde"
-session = sa.create_session(user_id)
-print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+        user_id_2 = "User 1"
+        session_id_2 = sa.create_session(user_id_2)
+        if session_id_2 is None:
+            print("create_session doesn't return a session ID when user_id is a string")
+            exit(1)
+        if sa.user_id_by_session_id is None:
+            print("user_id_by_session_id should not be None")
+            exit(1)
+        if type(sa.user_id_by_session_id) is not dict:
+            print("user_id_by_session_id should be a dictionary: {}".format(sa.user_id_by_session_id))
+            exit(1)
+        r_user_id_2 = sa.user_id_by_session_id.get(session_id_2)
+        if r_user_id_2 != user_id_2:
+            print("User doesn't found for this session ID: {} -> {}".format(session_id_2, sa.user_id_by_session_id))
+            exit(1)
+        
+        if session_id_1 == session_id_2:
+            print("Same User should not have the same session ID if we create twice a session: {}".format(sa.user_id_by_session_id))
+            exit(1)
+        print("OK", end="")
+    except:
+        import sys
+        print("Error: {}".format(sys.exc_info()))
