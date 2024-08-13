f6d67455d3f793c64c3d68788712058a0be47682
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-13 09:19:39 +0100

    implement create user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -14,8 +14,6 @@ class DB:
     """DB class
     """
 
-
-
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
@@ -33,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
         new_user = User(email=email, hashed_password=hashed_password)
